99ad2c6a2575900d2a7aa30f6ffffaeeb770de30
SerNerf <shaydurov2000@yandex.ru>
2021-03-31 12:52:28 +0300

    Сделал адаптив блока conferences

--- app/js/main.js
+++ app/js/main.js
@@ -8,6 +8,23 @@ $(function(){
         centerMode: true,
         slidesToShow: 3,
         slidesToScroll: 1,
+        responsive: [
+        {
+            breakpoint: 950,
+            settings: {
+            slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+            arrows: false,
+            dots: true,
+            centerMode: false,
+            slidesToShow: 1
+            }
+        }
+        ]
     });
 
 });
